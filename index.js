942164eb21ec7e6f63ba2c45594bc6a3165fe2c2
Faria-123 <mrsnoushin1987@gmail.com>
2026-08-14 03:49:31 +0600

    here booking part done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+// app.use(express.json());
 
 
 const uri = process.env.MONGODB_URI;
@@ -24,7 +25,9 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const db = client.db('systemapp');
+
         const dataCollections = db.collection('sports');
+        const bookingCollections = db.collection('bookings');
         app.get('/sports', async (req, res) => {
             const cursor = dataCollections.find().limit(6);
             const result = await cursor.toArray();
@@ -35,6 +38,35 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+        // app.post('/bookings', async (req, res) => {
+        //     const bookingData = req.body;
+        //     const result = await bookingCollections.insertOne(bookingData).toArray();
+        //     res.json(result);
+        // })
+        app.post("/bookings", async (req, res) => {
+            try {
+                const bookingData = req.body;
+
+                console.log("Received booking:", bookingData);
+
+                const result = await bookingCollections.insertOne(bookingData);
+                // res.send(result);
+                res.status(201).json({
+                    success: true,
+                    message: "Booking created successfully",
+                    data: result,
+                });
+
+            } catch (error) {
+                console.error("Booking error:", error);
+
+                res.status(500).json({
+                    success: false,
+                    message: "Booking failed",
+                    error: error.message,
+                });
+            }
+        });
         app.get('/allsports/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
@@ -43,6 +75,16 @@ async function run() {
             const result = await dataCollections.findOne(query);
             res.send(result);
         });
+        app.get('/bookings/:userId', async (req, res) => {
+            const { userId } = req.params;
+            const result = await bookingCollections.find({ userId: userId }).toArray();
+            res.send(result);
+        });
+        app.delete("/bookings/:bookingId", async (req, res) => {
+            const { bookingId } = req.params;
+            const result = await bookingCollections.deleteOne({ _id: new ObjectId(bookingId) });
+            res.json(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
